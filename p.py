fd618e6617a54be146951e0fd2ea323e04532fe6
Duel <51247353+marteiduel@users.noreply.github.com>
2023-05-29 22:32:22 -0600

    extracting info from pdfs (not complete)

--- p.py
+++ p.py
@@ -1,32 +1,7 @@
 import csv
 import re
-
-pdf_text = """
-5/29/23, 5:25 PM Order details - Walmart.com
-https://www.walmart.com/orders/200011058419487 1/2
-Progresso, Italian Style Bread Crumbs, 15 oz. SubstitutionsQty 1 $2.08
-Great Value Italian Style Panko Bread Crumbs, 8 oz UnavailableQty 1 $1.77
-Navel Oranges, 4 lb Bag Weight-adjustedQty 1 $4.98
-Fresh Roma Tomato, Each Weight-adjustedQty 3 $0.86
-Spring Valley Vitamin C with Rose Hips Tablets Dietary Supplement, 1,000 mg, 250
-CountShoppedQty 1 $9.88
-Equate Daily Fiber, 100% Psyllium Husk Dietary Supplement Capsules, 300 CountShoppedQty 1 $10.98
-McCormick Garlic Powder, 3.12 oz ShoppedQty 1 $4.23
-Breyers Frozen Dairy Dessert Cookies & Cream Ice Cream Alternative, 48 oz ShoppedQty 1 $4.97
-Great Value Finely Shredded Fiesta Blend Cheese, 32 oz ShoppedQty 1 $7.48
-Great Value Long Grain Enriched Rice, 5 lbs ShoppedQty 2 $5.88
-Fresh Green Onions Bunch, Each ShoppedQty 1 $0.88
-Great Value Chicken Broth, 32 oz ShoppedQty 2 $2.74May 27, 2023 order
-Order# 2000110-58419487
-Subtotal $54.96
-Free delivery from store  $7.95$0
-Tax $2.11
-Driver tip $0.00
-5/29/23, 5:25 PM Order details - Walmart.com
-https://www.walmart.com/orders/200011058419487 2/2Total $57.07
-Payment method
- Ending in 9975
-"""
+import glob
+import PyPDF2
 
 # Define keywords for identifying item lines
 keywords = [
@@ -38,37 +13,53 @@ keywords = [
 ]
 
 # Regular expression patterns
-item_line_pattern = r"^(.+?)\s+({})\s+(\d+)\s+\$([\d.]+)$".format("|".join(keywords))
+item_line_pattern = r"^(.+?)\s+({})\s+(\d+)\s+\$([\d.]+)$".format(
+    "|".join(keywords))
 date_pattern = r"\b(\d{1,2}/\d{1,2}/\d{2})\b"
 
-# Initialize empty list to store item data
-items = []
+# Retrieve a list of PDF file paths in the current folder
+pdf_files = glob.glob("*.pdf")
 
-# Extract item data from the PDF text
-lines = pdf_text.split("\n")
-current_date = None
-
-for line in lines:
-    line = line.strip()
-    if not line:
-        continue
-
-    # Extract date from the line
-    date_match = re.search(date_pattern, line)
-    if date_match:
-        current_date = date_match.group(1)
-        continue
-
-    # Check if the line represents an item
-    item_match = re.match(item_line_pattern, line)
-    if item_match:
-        item_name = item_match.group(1)
-        quantity = item_match.group(3)
-        price = item_match.group(4)
-
-        items.append([item_name, quantity, price, current_date])
-
-# Save item data to a CSV file
-with open("items.csv", "w", newline="") as csvfile:
+# Create a CSV file to consolidate all the results
+csv_file = "consolidated_results.csv"
+with open(csv_file, "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Item Name", "Quantity", "Price", "Date"])
+
+    # Process each PDF file
+    for pdf_file in pdf_files:
+        # Read the PDF file and extract text
+        with open(pdf_file, "rb") as file:
+            reader = PyPDF2.PdfReader(file)
+            pdf_text = ""
+            for page in reader.pages:
+                pdf_text += page.extract_text()
+
+        # Extract item data from the PDF text
+        lines = pdf_text.split("\n")
+        current_date = None
+
+        for line in lines:
+            line = line.strip()
+            if not line:
+                continue
+
+            # Extract date from the line
+            date_match = re.search(date_pattern, line)
+            if date_match:
+                current_date = date_match.group(1)
+                continue
+
+            # Check if the line represents an item
+            item_match = re.match(item_line_pattern, line)
+            if item_match:
+                item_name = item_match.group(1)
+                quantity = item_match.group(3)
+                price = item_match.group(4)
+
+                # Append item data to the consolidated CSV file
+                writer.writerow([item_name, quantity, price, current_date])
+
+        print(f"Processed {pdf_file} and appended item data to {csv_file}.")
+
+print(f"All PDF files processed. Consolidated results saved to {csv_file}.")
---
+++ specific.py
@@ -0,0 +1,23 @@
+import PyPDF2
+
+# Specify the path of the PDF file to open
+pdf_file_path = "3.pdf"
+
+# Open the output text file
+with open("3printPDF.txt", "w") as output_file:
+    # Open the PDF file
+    with open(pdf_file_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        line_number = 1
+        for page in reader.pages:
+            # Extract text from each page
+            text = page.extract_text()
+            # Split the text into lines
+            lines = text.split("\n")
+            # Write each line with a line number to the output file
+            for line in lines:
+                output_file.write(f"{line_number} {line}\n")
+                line_number += 1
+
+# Print a message when done
+print("Text content extracted and saved to 'printPDF.txt'.")
