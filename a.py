125365f9ecc12b065d634c1faedda320a3203af7
Duel <51247353+marteiduel@users.noreply.github.com>
2023-06-20 15:17:12 -0600

    exception handling and creating json file

--- a.py
+++ a.py
@@ -1,10 +1,10 @@
+import json
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import time
-from bs4 import BeautifulSoup
 
 # Path to your chromedriver executable
 chrome_driver_path = 'C:/path/to/chromedriver.exe'
@@ -14,13 +14,12 @@ chrome_profile_path = 'C:/path/to/Chrome/Profile'
 
 chrome_options = Options()
 chrome_options.add_argument("user-data-dir=" + chrome_profile_path)
-
-# Additional options if needed
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-driver = webdriver.Chrome(
-    executable_path=chrome_driver_path, options=chrome_options)
+service = Service(chrome_driver_path)
+
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 url = "https://www.walmart.com/account/login?vid=oaoh"
 
@@ -50,26 +49,50 @@ view_details_buttons = driver.find_elements(
 
 links = [button.get_attribute("href") for button in view_details_buttons]
 
+product_list = []
+
 for link in links:
+    time.sleep(1)
     driver.get(link)
+    time.sleep(1)
+    date = driver.find_element(
+        By.XPATH, '//h1[@class="w_kV33 w_LD4J w_mvVb f3 f-subheadline-m di-m dark-gray-m print-bill-date lh-copy"]').text
+    print(date)
     arrow_down = driver.find_element(By.CLASS_NAME, "relative.mr3")
     arrow_down.click()
-    accordeon = link.find(
-        'div', attrs={'data-testid': 'category-accordion-Shopped'})
+    try:
+        accordeon = driver.find_element(
+            By.XPATH, '//div[@data-testid="category-accordion-Shopped"]')
+        products = accordeon.find_elements(
+            By.XPATH, '//div[contains(@class, "pa3 pb0 ph4-m")]')
+        time.sleep(1)
+        for product in products:
+            try:
+                product_name = product.find_element(
+                    By.XPATH, './/div[@data-testid="productName"]').text
+                product_quantity = product.find_element(
+                    By.XPATH, './/div[@class="pt1 f7 f6-m bill-item-quantity gray"]').text
+                product_price = product.find_element(
+                    By.XPATH, './/span[@aria-hidden="false"]').text
+                print(product_name)
+                print(product_quantity)
+                print(product_price)
+                product_data = {
+                    "name": product_name,
+                    "quantity": product_quantity,
+                    "price": product_price,
+                    "date": date
+                }
 
-products = accordeon.find_all('div', attrs={'class': 'pa3 pb0 ph4-m'})
+                product_list.append(product_data)
 
-total_number_of_products = len(products)
+            except NoSuchElementException:
+                print("Some elements not found for the current product.")
+                continue
 
-for product in products:
-    product_name = product.find(
-        'div', attrs={'data-testid': "productName"}).text
-    product_quantity = product.find(
-        'div', attrs={'class': "pt1 f7 f6-m bill-item-quantity gray"}).text
-    product_price = product.find('span', attrs={'aria-hidden': "false"}).text
-    print(product_name)
-    print(product_quantity)
-    print(product_price)
+    except NoSuchElementException:
+        print("Accordion element not found for the current link.")
+        continue
 
-
-time.sleep(300)
+with open("raw_product_list.json", "w") as file:
+    json.dump(product_list, file, indent=4)
